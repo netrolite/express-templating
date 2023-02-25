9d1f30d815fefb225f7dfb95729bd7e7c9ba1413
Matvey <morozovmatvej75@gmail.com>
2023-02-25 21:58:10 +0300

    make main template & add index, about, notFound pages

--- src/index.ts
+++ src/index.ts
@@ -3,11 +3,19 @@ import { engine } from "express-handlebars";
 
 const app = express();
 
-app.engine("handlebars", engine());
+app.engine("handlebars", engine({ defaultLayout: "main" }));
 app.set("view engine", "handlebars");
 
 app.get("/", (req, res) => {
   res.render("index");
+})
+
+app.get("/about", (req, res) => {
+  res.render("about");
+})
+
+app.use((req, res) => {
+  res.render("notFound");
 })
 
 const PORT = process.env.PORT || 3000;
